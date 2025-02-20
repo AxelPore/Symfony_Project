902b0aed4f1bfeca448124ce19b5886d62890881
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-02-20 10:46:30 +0100

    link and style terms of use page

--- src/Controller/TermsController.php
+++ src/Controller/TermsController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class TermsController extends AbstractController
 {
     #[Route('/terms', name: 'app_terms')]
-    #[IsGranted('PUBLIC_ACCESS')]
     public function index(): Response
     {
         $user = $this->getUser();
